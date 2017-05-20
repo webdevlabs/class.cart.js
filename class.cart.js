ec06f0f05413dc65d0b4575fbf0ec21f52a74214
Web Development Labs <webdevlabs@users.noreply.github.com>
2017-05-20 10:32:19 +0300

    added comments

--- class.cart.js
+++ class.cart.js
@@ -5,9 +5,11 @@
 class Cart {
 
     constructor() {
+        // fetch cartStorage object from sessionStorage
         this.cartStorage = JSON.parse(sessionStorage.getItem('cart'));
         if (!this.cartStorage) { this.cartStorage = []; }
         this.renderCart();
+        // attach show/hide cart button
         document.querySelector('#shoppingCart .cartBtn').addEventListener('click', function(event) {
             if (document.querySelector('#shoppingCart ul').style.display === 'block') {
                 document.querySelector('#shoppingCart ul').style.display = 'none';
@@ -18,6 +20,7 @@ class Cart {
     }
 
     saveCart() {
+        // save cartStorage object to local browser sessionStorage
         sessionStorage.setItem('cart', JSON.stringify(this.cartStorage));
     }
 
@@ -74,35 +77,41 @@ class Cart {
         var cartTotal = Number('0');
         var itemQtyBox = '';
         document.querySelector('#shoppingCart ul').innerHTML = '';
+        // Render Cart view items from cartStorage object
         this.cartStorage.forEach(function(item) {
             if (item.qty > 1) { itemQtyBox = item.qty + ' x '; } else { itemQtyBox = ''; }
             var li = document.createElement("li");
-            li.setAttribute("title", 'Премахни');
+            li.setAttribute("title", 'click to remove');
             li.setAttribute("data-price", item.price);
             li.setAttribute("data-itemid", item.id);
             li.appendChild(document.createTextNode(itemQtyBox + item.title));
+            // attach on click remove item from cart
             li.addEventListener('click', function(event) {
                 window.cart.removeItem(item.id);
             });
             document.querySelector('#shoppingCart ul').appendChild(li);
+            // increase the cartTotal amount
             cartTotal = Number(cartTotal + (Number(item.price) * Number(item.qty)));
         });
         if (cartTotal > 0) {
             document.querySelector('#shoppingCart').style.display = 'block';
-            document.querySelector('#cartTotal').innerHTML = cartTotal.toFixed(2) + 'лв';
+            document.querySelector('#cartTotal').innerHTML = cartTotal.toFixed(2) + 'USD';
         } else {
+            // hide cart if no items
             document.querySelector('#shoppingCart').style.display = 'none';
         }
     }
 
 }
 
+// Create the Cart object when the page is loaded
 document.onreadystatechange = function() {
     if (document.readyState == 'complete') {
         window.cart = new Cart();
     }
 };
 
+// Attach class .add2cart button functionality
 document.body.addEventListener('click', function(e) {
     var t = e.target;
     if (t.classList.contains('add2cart')) {
