e2e6adaf21470713871232b00e59638758bd6b5c
Web Development Labs <webdevlabs@users.noreply.github.com>
2017-06-12 10:07:09 +0300

    use map instead of forEach

--- class.cart.js
+++ class.cart.js
@@ -83,7 +83,7 @@ class Cart {
         var currencySymbol = this.currencySymbol;
         document.querySelector('#shoppingCart ul').innerHTML = '';
         // Render Cart view items from cartStorage object        
-        this.cartStorage.forEach(function(item) {
+        this.cartStorage.map(function(item) {
             if (item.qty > 1) { itemQtyBox = `${item.qty} x`; } else { itemQtyBox = ''; }
             let li = document.createElement("li");
             li.setAttribute("title", removeItemTitle);
